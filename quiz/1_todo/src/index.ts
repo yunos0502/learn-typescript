a3224ccf64899476bc6679e4fe4b9d860e18484b
Park Gyu Ae <yunos0502@gmail.com>
2021-06-01 23:50:41 +0900

    첫번째 프로젝트: 할일 관리 애플리케이션

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,19 @@
-let todoItems;
+// type TodoProps = {
+//   id: number, 
+//   title: string, 
+//   done: boolean
+// };
+
+interface TodoProps {
+  id: number,
+  title: string;
+  done: boolean;
+}
+
+let todoItems: TodoProps[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): TodoProps[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,40 +23,42 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): TodoProps[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: TodoProps): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: TodoProps): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): TodoProps {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): TodoProps[] {
   return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  addTodo({ id: 4, title: '4', done: false });
+  addTodo({ id: 5, title: '5', done: false });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
